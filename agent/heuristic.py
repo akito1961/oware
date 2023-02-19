b56e4f8bba8b59f097afe124a6e3597a8ab88e0e
akito1961 <98795718+akito1961@users.noreply.github.com>
2023-02-19 23:50:58 +0700

    heuristic draft

---
+++ agent/heuristic.py
@@ -0,0 +1,18 @@
+
+def _h1(): # right-most pit seed possible
+    pass
+
+def _h2(): # seed on the owner side
+    pass
+
+def _h3(): # possible move diversity and maintaining options
+    pass
+
+def _h4(): # max score gain for AI
+    pass
+
+def _h5(): # right-most pit availability
+    pass
+
+def _h6(): # min score gain for player
+    pass
